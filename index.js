226c85f5faf00d8faee509993ae4e7dd589c0f6f
phamtruong <truongpd.doublenine@gmail.com>
2020-05-14 14:31:03 +0700

    add-index-f1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 console.log("hello branch f2");
 
 // change abcdef
 
 // change 2
 //  change after rebase master /dev
+=======
+console.log('hello branch f2');
+>>>>>>> add-index-f1
